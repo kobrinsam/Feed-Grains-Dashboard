e7dc7f0da26dbd23424d0bca68023422dba21f39
kobrinsam <60291549+kobrinsam@users.noreply.github.com>
2022-09-02 18:18:35 -0400

    first draft

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,111 +7,60 @@ import yfinance as yf
 from sklearn import preprocessing
 import numpy as np
 import requests
-st.title("American Agricultural Economy Barometer")
-st.subheader("Surveying the state of the agriculture industry on a monthly basis")
-st.caption("Data sources include Ag Economy Barometer Indices produced by Perdue Univeristy and key Agricultural input and output prices.")
-### format ag barometer data
-df = pd.read_html("https://ag.purdue.edu/commercialag/ageconomybarometer/tables/")[0]
-df['Month/Year.1'] = df['Month/Year.1'].apply(lambda x: str(x))
-df['Date'] = df['Month/Year'] +'-'+ df['Month/Year.1']
+st.title("USDA Economic Reseach Service Feed Grains Dashboard")
+st.caption("This dashboard contains statistics on four feed grains (corn, grain sorghum, barley, and oats), foreign coarse grains (feed grains plus rye, millet, and mixed grains), hay, and related items. This includes data published in the monthly Feed Outlook and previously annual Feed Yearbook. Data are monthly, quarterly, and/or annual depending upon the data series. Latest data may be preliminary or projected. Missing values indicate unreported values, discontinued series, or not yet released data.")
+import pandas as pd
+import zipfile
+zip_file_url = "https://www.ers.usda.gov/webdocs/DataFiles/50048/FeedGrains.zip?v=7455.2"
+import requests, zipfile, io
+
+#import data
+r = requests.get(zip_file_url, stream=True)
+z = zipfile.ZipFile(io.BytesIO(r.content))
+z.extractall()
+df = pd.read_csv(z.open('FeedGrains.csv'))
+
+##function to format date
+def date_format(freq, year, time, time_id):
+  if freq == 1:
+      return time +'-'+ year
+  elif freq == 2:
+    if str(time_id)[-1] == "1":
+     return "Jan" +'-'+ year
+    elif str(time_id)[-1] == "2":
+      return "Apr" +'-'+ year
+    elif str(time_id)[-1] == "3":
+      return "July" +'-'+ year
+    else:
+      return "Oct" +'-'+ year
+  elif freq == 3:
+    return "Jan" +'-'+ year
+
+#format dates
+df['Year_ID'] = df['Year_ID'].apply(lambda x: str(x))
+df['Date'] = df.apply(lambda x: date_format(x["SC_Frequency_ID"], x["Year_ID"], x["Timeperiod_Desc"], x["Timeperiod_ID"]), axis=1)
 df['Date'] = pd.to_datetime(df['Date'])
-df = df.drop(['Month/Year', 'Month/Year.1'], axis=1)
-#######
-### get yahoo finance data
-data = yf.download(
-        # tickers list or string as well
-        tickers = "UFV=F ZC=F ZS=F LE=F DC=F HE=F NG=F",
+### sidebar
+with st.sidebar:
+    df = df[df.SC_GeographyIndented_Desc == "United States"]
+    group = st.multiselect('Group', list(df.SC_Group_Desc.unique()), default='Prices')
+    df_group = df[df.SC_Group_Desc.isin(group)]
+    comm = st.multiselect('Commodity', list(df_group.SC_GroupCommod_Desc.unique()), default='Hay')
+    comm_group = df_group[df_group.SC_GroupCommod_Desc.isin(comm)]
+    attr = st.multiselect('Data Attribute', list(comm_group.SC_Attribute_Desc.unique()), default='Prices received by farmers')
+    attr_df = comm_group[comm_group.SC_Attribute_Desc.isin(attr)]
+    dateFreq = st.radio('Frequency', list(attr_df.SC_Frequency_Desc.unique()))
+    freq_df = comm_group[attr_df.SC_Frequency_Desc == dateFreq]
+#filters
+chart_title = "{} {}, {}".format(comm[0], attr[0], freq_df.SC_Unit_Desc.min())
+st.subheader(chart_title)
+st.line_chart(data = freq_df, x= 'Date', y='Amount')
+table_title = "Data Table: {} {}".format(comm[0], attr[0])
+st.text(table_title)
+st.dataframe(freq_df[['Date', "Amount"]])
 
-        #LE=F is live cattle futures
-        # DC=F is milk futures
-        # HE=F is lean hog futures
-        period = "max",
-        interval = "1mo",
-        group_by = 'column',
-        auto_adjust = True,
-        prepost = True,
-    )
-data = data['Close'].rename(columns={"DC=F":"Milk Futures (DC-F)", "HE=F" :"Lean Hog Futures (HE=F)",
-                              "LE=F": "Live Cattle Futures (LE=F)",
-                              "UFV=F":"Urea (Granular) FOB US Gulf Fut (UFV=F)",
-                              "ZS=F":"Soybean Futures,Nov-2022 (ZS=F)",
-                              "NG=F": "Natural Gas Oct 22 (NG=F)",
-                              "ZC=F":"Corn Futures,Dec-2022 (ZC=F)"})
-
-
-#### get drought data
-r = requests.get("https://usdmdataservices.unl.edu/api/USStatistics/GetDroughtSeverityStatisticsByArea?aoi=conus&startdate=1/1/2000&enddate=8/1/2022&statisticsType=1")
-json = r.json()
-drought = pd.DataFrame.from_dict(json)
-drought[['D0', 'D1', 'D2', 'D3','D4']] = drought[['D0', 'D1', 'D2', 'D3','D4']].applymap(lambda x: float(str.replace(x,',', '')))
-drought['Date'] = pd.to_datetime(drought['MapDate'])
-drought = drought.set_index('Date').resample("M").mean()
-drought = drought.reset_index()
-drought['Date'] = drought['Date'].apply(lambda x: x.replace(day=1))
-drought['CONUS Area in Drought (D1 - D4)'] = drought['D1'] + drought['D2'] + drought['D3'] + drought['D4']
-drought = drought.rename(columns = {'D0' : 'CONUS Area at Risk of Drought - D0',
- 'D1':'CONUS Area in Moderate Drought - D1',
-  'D2': 'CONUS Area in Severe Drought - D2',
-   'D3': 'CONUS Area in Extreme Drought - D3',
-    'D4':'CONUS Area in Exceptional Drought - D4'})
-#merge dataframes
-total_data = data.reset_index().merge(df, how='left').merge(drought, how='left')
-# normalize data
-#create scaled dataframe
-total_data = total_data.set_index('Date')
-scaled_df = preprocessing.normalize(total_data.fillna(0), axis=0)
-scaled_df = pd.DataFrame(scaled_df, columns=total_data.columns, index =total_data.index).replace({0:np.NaN}).reset_index()
-total_data = total_data.reset_index()
-barometer_list = ['Purdue/CME Ag Economy Barometer',
- 'Index of Current Conditions',
- 'Index of Future Expectations',
- 'Farm Capital Investment Index']
-drought_list =  ['CONUS Area at Risk of Drought - D0',
-'CONUS Area in Moderate Drought - D1',
-  'CONUS Area in Severe Drought - D2',
-   'CONUS Area in Extreme Drought - D3',
-    'CONUS Area in Exceptional Drought - D4',
-    'CONUS Area in Drought (D1 - D4)']
-####
-# correlations df
-corr_df = scaled_df[scaled_df['Date']>'2010-10-01'].rename(columns={"DC=F":"Milk Futures (DC-F)", "HE=F" :"Lean Hog Futures (HE=F)",
-                              "LE=F": "Live Cattle Futures (LE=F)",
-                              "UFV=F":"Urea (Granular) FOB US Gulf Fut (UFV=F)",
-                              "ZS=F":"Soybean Futures,Nov-2022 (ZS=F)",
-                              "NG=F": "Natural Gas Oct 22 (NG=F)",
-                              "ZC=F":"Corn Futures,Dec-2022 (ZC=F)"}).corr().drop(barometer_list, axis=0)
-
-## drought df
-drought_df = corr_df['CONUS Area in Drought (D1 - D4)'].drop(drought_list, axis=0)
-
-
-###functions
-def display_data(norm):
-    if norm:
-        return scaled_df
-    else :
-        return total_data
-#frontend
-
-indicators = st.multiselect('Agricultural Economic Indicators', list(total_data.drop('Date', axis=1).columns))
-norm = st.checkbox('Normalize Economic Indicators')
-st.text("Ag Economy Indicators, by month")
-st.line_chart(data = display_data(norm), x= 'Date' , y = indicators)
-st.text("Ag Economy Indicator Data Table")
-st.dataframe(display_data(norm).set_index('Date'))
-st.subheader('Farmer Sentiment is Associated with Key Agricultural Commodity Prices')
-st.text('Correlation Between Ag Economy Barometer Indices and Key Ag Commodity Prices')
-radio = st.radio('Ag Barometer Index', ['Purdue/CME Ag Economy Barometer',
- 'Index of Current Conditions',
- 'Index of Future Expectations',
- 'Farm Capital Investment Index'], index=0, horizontal=True)
-bar_df = corr_df[radio].sort_values()
-st.bar_chart(data=bar_df)
-st.subheader('There is a Strong Relationship Between Drought and Ag Commodity Futures Prices')
-st.text('Correlation Between CONUS Area in drought and Key Ag Commodity Prices')
-st.bar_chart(data= drought_df.sort_values())
-col1, col2 = st.columns([4, 1])
-st.text('Data Sources: Purdue Ag Barometer, The U.S. Drought Monitor, and Yahoo Finance')
+##footer
+st.text('Data Sources: Feed Grains: Yearbook Table, USDA Economic Reseach Service')
 st.text('By Sam Kobrin')
 
 
